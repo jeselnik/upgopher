53d0408da4e8b2069fbb8ee72d59513ad9eb9e55
Edward Jeselnik <eddie@jeselnik.xyz>
2023-02-13 19:56:53 +1100

    Reformat token for header value

--- upgopher.go
+++ upgopher.go
@@ -3,9 +3,9 @@ package upgopher
 const baseURL = "https://api.up.com.au/api/v1"
 
 type upAPI struct {
-	t string
+	headerVal string
 }
 
 func NewUp(token string) upAPI {
-	return upAPI{"token"}
+	return upAPI{"Bearer:" + "token"}
 }
