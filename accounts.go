50ecf7d27a64fa2688d27b1d3e380a9166ac2ddc
Edward Jeselnik <eddie@jeselnik.xyz>
2023-02-14 21:42:11 +1100

    Build struct from accounts sample response

--- accounts.go
+++ accounts.go
@@ -1,3 +1,41 @@
 package upgopher
 
 const accountsBase = baseURL + "/accounts"
+
+type AccountsList struct {
+	Data []struct {
+		Type string
+		ID   string
+
+		Attributes struct {
+			DisplayName   string
+			AccountType   string
+			OwnershipType string
+
+			Balance struct {
+				CurrencyCode     string
+				Value            string
+				ValueInBaseUnits string
+			}
+
+			CreatedAt string
+		}
+
+		Relationships struct {
+			Transactions struct {
+				Links struct {
+					Related string
+				}
+			}
+
+			Links struct {
+				Self string
+			}
+		}
+	}
+
+	Links struct {
+		Prev string
+		Next string
+	}
+}
