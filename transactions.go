180d230a410dd598abe56f09f61e4a9137882d5c
Edward Jeselnik <eddie@jeselnik.xyz>
2023-02-18 23:25:11 +1100

    Transaction List struct

--- transactions.go
+++ transactions.go
@@ -1,3 +1,75 @@
 package upgopher
 
 const transBase = baseURL + "/transactions"
+
+type TransactionList struct {
+	Data []struct {
+		Type string
+		ID   string
+
+		Attributes struct {
+			Status          string
+			RawText         string
+			Description     string
+			Message         string
+			IsCategorizable bool
+			HoldInfo        string
+			RoundUp         string
+			Cashback        string
+
+			Amount struct {
+				CurrencyCode     string
+				Value            string
+				ValueInBaseUnits int
+			}
+
+			ForeignAmount      string
+			CardPurchaseMethod string
+			SettledAt          string
+			CreatedAt          string
+		}
+
+		Relationships struct {
+			Account struct {
+				Data struct {
+					Type string
+					ID   string
+				}
+				Links struct {
+					Related string
+				}
+
+				Category struct {
+					Data  string
+					Links struct {
+						Self string
+					}
+
+					ParentCategory struct {
+						Data string
+					}
+
+					Tags struct {
+						Data []struct {
+							Type string
+							ID   string
+						}
+
+						Links struct {
+							Self string
+						}
+					}
+				}
+			}
+		}
+
+		Links struct {
+			Self string
+		}
+	}
+
+	Links struct {
+		Prev string
+		Next string
+	}
+}
