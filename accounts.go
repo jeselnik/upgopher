40f7271a8e5b71cb520a07f61697e2b3485eea86
Edward Jeselnik <eddie@jeselnik.xyz>
2023-04-24 22:34:41 +1000

    Remove redundant declaration of Account struct in list

--- accounts.go
+++ accounts.go
@@ -5,36 +5,7 @@ import "fmt"
 const accountsBase = baseURL + "/accounts"
 
 type AccountsList struct {
-	Data []struct {
-		Type string `json:"type"`
-		ID   string `json:"id"`
-
-		Attributes struct {
-			DisplayName   string `json:"displayName"`
-			AccountType   string `json:"accountType"`
-			OwnershipType string `json:"ownershipType"`
-
-			Balance struct {
-				CurrencyCode     string `json:"currencyCode"`
-				Value            string `json:"value"`
-				ValueInBaseUnits int    `json:"valueInBaseUnits"`
-			} `json:"balance"`
-
-			CreatedAt string `json:"createdAt"`
-		} `json:"attributes"`
-
-		Relationships struct {
-			Transactions struct {
-				Links struct {
-					Related string `json:"related"`
-				} `json:"links"`
-			} `json:"transactions"`
-
-			Links struct {
-				Self string
-			}
-		} `json:"relationships"`
-	} `json:"data"`
+	Data []Account `json:"data"`
 
 	Links struct {
 		Prev string `json:"prev"`
