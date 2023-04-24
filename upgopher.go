0472fe04b1e67e84f49729e6f63460a34a72b3a6
Edward Jeselnik <eddie@jeselnik.xyz>
2023-04-25 00:08:08 +1000

    upgopher moves to reusable

--- upgopher.go
+++ upgopher.go
@@ -1,12 +1,5 @@
 package upgopher
 
-import (
-	"encoding/json"
-	"io"
-	"net/http"
-	"time"
-)
-
 const (
 	baseURL   = "https://api.up.com.au/api/v1"
 	headerKey = "Authorization"
@@ -21,45 +14,11 @@ func Up(token string) Bearer {
 	return Bearer{"Bearer " + token}
 }
 
-func newRequest(url string, b Bearer) ([]byte, error) {
-	client := &http.Client{Timeout: 5 * time.Second}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set(headerKey, b.Val)
-	res, resErr := client.Do(req)
-	if resErr != nil {
-		return nil, resErr
-	}
-	defer res.Body.Close()
-	body, readErr := io.ReadAll(res.Body)
-	if readErr != nil {
-		return nil, readErr
-	}
-
-	return body, nil
-}
-
 type PingRes struct {
 	Meta struct {
 		ID          string `json:"id"`
 		StatusEmoji string `json:"statusEmoji"`
 	} `json:"meta"`
-}
-
-type response interface {
-	PingRes | AccountsList | Account | TransactionList
-}
-
-func get[T response](list *T, url string, b Bearer) error {
-	res, err := newRequest(url, b)
-	if err != nil {
-		return err
-	}
-
-	jsonErr := json.Unmarshal(res, list)
-	return jsonErr
 }
 
 func Ping(b Bearer) (PingRes, error) {
