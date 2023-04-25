e312722623a15785d893702318be70869de97a83
Edward Jeselnik <eddie@jeselnik.xyz>
2023-04-25 12:12:41 +1000

    define tag types

--- tags.go
+++ tags.go
@@ -1,3 +1,19 @@
 package upgopher
 
 const tagBase = baseURL + "/tags"
+
+type TagResource struct {
+	Type string `json:"type"`
+	ID   string `json:"id"`
+
+	Relationships struct {
+		Transactions struct {
+			LinkToRelated `json:"links"`
+		} `json:"transactions"`
+	} `json:"relationships"`
+}
+
+type TagList struct {
+	Tag   TagResource `json:"data"`
+	Links NavLinks    `json:"links"`
+}
