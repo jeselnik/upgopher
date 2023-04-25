d712bef6b581488f657a8463939e961cd56c1ab8
Edward Jeselnik <eddie@jeselnik.xyz>
2023-04-25 10:49:27 +1000

    Define category structs

--- categories.go
+++ categories.go
@@ -1,3 +1,33 @@
 package upgopher
 
 const catBase = baseURL + "/categories"
+
+type CategoryResource struct {
+	Type string `json:"type"`
+	ID   string `json:"id"`
+
+	Attributes struct {
+		Name string `json:"name"`
+	} `json:"attributes"`
+
+	Relationships struct {
+		Parent struct {
+			Data  DataObject    `json:"data"`
+			Links LinkToRelated `json:"related"`
+		} `json:"parent"`
+		Children struct {
+			Data  []DataObject  `json:"data"`
+			Links LinkToRelated `json:"related"`
+		} `json:"children"`
+	} `json:"relationships"`
+
+	Links LinkToSelf `json:"links"`
+}
+
+type Category struct {
+	Data CategoryResource `json:"data"`
+}
+
+type CategoryList struct {
+	Data []CategoryResource `json:"data"`
+}
